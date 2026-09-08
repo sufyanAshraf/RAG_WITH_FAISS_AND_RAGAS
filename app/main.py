97e1ceafc15d905a30a29182cb68ac2c2469d806
Sufyan <44521060+sufyanAshraf@users.noreply.github.com>
2026-09-08 21:59:47 +0500

    integrating new data loading and embaddings

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel 
 import configparser 
-from .logger import logger 
-from .ingession import read_documents_from_file
+from .logger import logger  
 from .models import GroqModel
 from .embeddingsModel import EmbeddingsModel  
 from langchain_community.vectorstores import FAISS
+from .readData import readData
+from .embeddingsCreator import create_embeddings
 
 def read_api_key_from_config() -> str:
     """Read the API key from the config.ini file."""
@@ -28,7 +29,7 @@ async def chat(request: chatRequest) -> chatResponse:
     groq_api_key, huggingface_api_key = read_api_key_from_config()
 
     model = GroqModel(groq_api_key)
-    docs = read_documents_from_file()
+    # docs = read_documents_from_file()
     embeddings_model = EmbeddingsModel()
     embedder = embeddings_model.get_model()  
 
@@ -36,58 +37,66 @@ async def chat(request: chatRequest) -> chatResponse:
     # logger.info(f"Has embed_documents: {hasattr(embedder, 'embed_documents')}")
     # logger.info(f"Has embed_query: {hasattr(embedder, 'embed_query')}")
 
-    # -------------------------
-    # 4. Create FAISS vector store
-    # -------------------------
+    # # -------------------------
+    # # 4. Create FAISS vector store
+    # # -------------------------
     
-    vector_store = FAISS.from_documents(
-        documents=docs,
-        embedding=embedder, 
-    )
-    logger.info("FAISS vector store created successfully")
-    # -------------------------
-    # 5. Search relevant documents
-    # -------------------------
-    relevant_docs = vector_store.similarity_search(
-        request.query,
-        k=3
-    )
+    # vector_store = FAISS.from_documents(
+    #     documents=docs,
+    #     embedding=embedder, 
+    # )
+    # logger.info("FAISS vector store created successfully")
+    # # -------------------------
+    # # 5. Search relevant documents
+    # # -------------------------
+    # relevant_docs = vector_store.similarity_search(
+    #     request.query,
+    #     k=3
+    # )
 
-    # -------------------------
-    # 6. Build context
-    # -------------------------
-    context = "\n\n".join(
-        doc.page_content
-        for doc in relevant_docs
-    )
+    # # -------------------------
+    # # 6. Build context
+    # # -------------------------
+    # context = "\n\n".join(
+    #     doc.page_content
+    #     for doc in relevant_docs
+    # )
 
-    # -------------------------
-    # 7. Create prompt
-    # -------------------------
-    full_prompt = f"""
-        You are a helpful AI assistant.
+    # # -------------------------
+    # # 7. Create prompt
+    # # -------------------------
+    # full_prompt = f"""
+    #     You are a helpful AI assistant.
 
-        Answer the user's question using the provided context.
+    #     Answer the user's question using the provided context.
 
-        Context:
-        {context}
+    #     Context:
+    #     {context}
 
-        Question:
-        {request.query}
+    #     Question:
+    #     {request.query}
 
-        Answer:
-    """
+    #     Answer:
+    # """
 
-    # -------------------------
-    # 8. Call Groq
-    # -------------------------
+    # # -------------------------
+    # # 8. Call Groq
+    # # -------------------------
      
-    response = model.invoke(
-        full_prompt=full_prompt
-    )
+    # response = model.invoke(
+    #     full_prompt=full_prompt
+    # )
  
+    read_data = readData()
+    data = read_data.readjson()
 
-    return chatResponse(response=response)
+    # Create embeddings for the data
+    embedding_records, vectors = create_embeddings(data, embedder)
+
+
+
+    response = {"message": "all ok"}
+    return chatResponse(response=response) 
 
 
 
